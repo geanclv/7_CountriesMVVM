bb1347e99b6d2381ad28fd86dc24e20ba30fb1e0
geanclv <geanclv@gmail.com>
2022-07-12 16:34:48 -0500

    Implementing the View: MainActivity, where we consume the ViewModel and Adapter created, in order to show the data on the UI.

--- app/src/main/java/com/geancarloleiva/a7_countriesmvvm/view/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/a7_countriesmvvm/view/MainActivity.kt
@@ -2,11 +2,76 @@ package com.geancarloleiva.a7_countriesmvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.geancarloleiva.a7_countriesmvvm.Country
 import com.geancarloleiva.a7_countriesmvvm.R
+import com.geancarloleiva.a7_countriesmvvm.viewmodel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ListViewModel
+    private lateinit var countryAdapter: CountryListAdapter
+    private lateinit var rviCountries: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initializing the ViewModel
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        //Initializing the adapter
+        countryAdapter = CountryListAdapter(this, arrayListOf()) { country ->
+            //itemClick events
+            Toast.makeText(this, "You clicked on ${country.name}", Toast.LENGTH_SHORT).show()
+        }
+
+        //Giving the behaviour to the RecyclerView
+        rviCountries = findViewById(R.id.rviCountries)
+        rviCountries.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countryAdapter
+        }
+
+        observeViewModel()
+    }
+
+    //Implementing the observer pattern (reactivity)
+    private fun observeViewModel(){
+        val lblErrorMessage: TextView = findViewById(R.id.lblErrorMessage)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+
+        //Observing the list of countries
+        viewModel.lstCountry.observe(this, Observer { lstCountry ->
+            lstCountry?.let {
+                countryAdapter.updateCountries(it)
+            }
+        })
+
+        //Observing the LoadError
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let {
+                lblErrorMessage.visibility = if(it) View.VISIBLE else View.GONE
+            }
+        })
+
+        //Observing the Loading
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                progressBar.visibility = if(it) View.VISIBLE else View.GONE
+                if(it){
+                    lblErrorMessage.visibility = View.GONE
+                    rviCountries.visibility = View.GONE
+                }
+            }
+        })
     }
 }
